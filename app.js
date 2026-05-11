87bf534a9d12cf9d0d67b3a189a5ec39eb59c969
Ujjwal5619P <ujjwal892360@gmail.com>
2026-05-11 21:00:31 +0530

    The project with ci/cd pipeline

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Starting API server...");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end("CI/CD Working");
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
